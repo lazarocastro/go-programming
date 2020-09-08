4e69202cd953606eefb9c00b0dfc3720c1a3bcb3
Lazaro Castro <lazarocastro@gmail.com>
2020-09-07 21:35:42 -0300

    Introduction to _ variabel and how to use it

---
+++ section4/26.introduction-to-packages/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, _ := fmt.Println("This is a new program", 42, true)
+	fmt.Println(n)
+
+	// n, err := fmt.Println("This is a new program", 42, true)
+	// fmt.Println(n)
+	// fmt.Println(err)
+}
+
+// https://golang.org/pkg/
