10c36229b5f7859fe6c0a99e425b161ae23630b8
Lazaro Castro <lazarocastro@gmail.com>
2020-09-07 21:03:49 -0300

    Hello World

---
+++ section4/25.hello-world/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello this is the my first program")
+	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
+}
+
+// control flow:
+// (1) sequence
+// (2) loop; interative
+// (3) conditional
